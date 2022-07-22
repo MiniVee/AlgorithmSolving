c4a3002a800570089267acbfc024778852fc1c84
Han Min <hm5395@naver.com>
2022-07-22 23:00:59 +0900

    BOJ Algorithm Solution

--- BOJ/막대기.py
+++ BOJ/막대기.py
@@ -1,2 +1,19 @@
-# BOJ 막대기 문제
+# 스택 - 막대기
+# https://www.acmicpc.net/problem/17608
 
+from sys import stdin
+input = stdin.readline
+
+N = int(input())
+NList = []
+answer = 1
+for i in range(N):
+    NList.append(int(input()))
+
+last = NList[-1]
+while len(NList):
+    now = NList.pop()
+    if now > last:
+        answer+=1
+        last = now
+print(answer)
